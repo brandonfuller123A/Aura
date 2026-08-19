72d114b9e3628abecc5be14ed272c3c3befd1a65
brandonfuller123A <brandonfuller98@gmail.com>
2026-08-19 07:07:13 +0100

    AbilitySystemComponent/AttributeSet

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -17,7 +17,6 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 public:
 	AAuraEnemy();
 	
-	
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
 	

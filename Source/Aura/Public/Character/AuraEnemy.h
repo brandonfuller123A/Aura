67bf6c9fdf6aeffcf89dc94599d6431d640efb4f
brandonfuller123A <brandonfuller98@gmail.com>
2026-09-07 13:29:33 +0100

    Player Level and Combat Interface

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -23,7 +23,13 @@ public:
 	virtual void UnHighlightActor() override;
 	/** end Enemy Interface */
 	
+	/** Combat Interface */
+	virtual int32 GetPlayerLevel() override;
+	/** end Combat Interface */
+	
 protected:
 	virtual void InitAbilityActorInfo() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
+	int32 Level = 1;
 };
